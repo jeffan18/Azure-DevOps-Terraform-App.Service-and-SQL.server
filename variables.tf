36b5831ba8623458b7ba613aefda8468b2377233
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-18 15:45:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,11 @@
-/* azure Credentials for Terraform */
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-
-/* Project, environment, location */
-locals {
-  storage_account_name = "${var.dns_prefix}${var.project}${substr(var.environment, 0, 3)}"
+variable "resource-group-name" {
+  default = "RG-terraform-test"
 }
 
-variable "environment" {
-  default = "dev"
-}
-
-variable "dns_prefix" {
-  default = "fan"
-}
-
-variable "project" {
-  default = "staticwebsite"
+variable "app-service-name" {
+  default = "terraform-test"
 }
 
 variable "location" {
-  default = "East US"
+  default = "eastus"
 }
-
-/* Resources */
-
-variable "resource_group_name" {
-  description = "resource group of the resources."
-  default = "RG-Fan-eastus-20200214"
-}
-
-
