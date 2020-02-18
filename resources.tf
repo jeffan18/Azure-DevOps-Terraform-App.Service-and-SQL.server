07ab44967a8bbc1ca994d0dd56d21e4450ff872e
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-18 15:43:15 -0500

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,19 +1,57 @@
-resource "azurerm_resource_group" "fanfan" {
-  name = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "test" {
+  name     = "${var.resource-group-name}"
+  location = "${var.location}"
 }
 
-resource "azurerm_storage_account" "fanfan" {
-  name                      = local.storage_account_name
-  resource_group_name       = azurerm_resource_group.fanfan.name
-  location                  = azurerm_resource_group.fanfan.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  enable_https_traffic_only = true
+resource "azurerm_app_service_plan" "test" {
+  name                = "example-appserviceplan"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
 
-  provisioner "local-exec" {
-    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.fanfan.name} --static-website  --index-document index.html --404-document 404.html"
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
+resource "azurerm_app_service" "test" {
+  name                = "${var.app-service-name}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "test" {
+  name                         = "terraform-sqlserver"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  location                     = "${azurerm_resource_group.test.location}"
+  version                      = "12.0"
+  administrator_login          = "test"
+  administrator_login_password = "Henan$heng10"
+}
+
+resource "azurerm_sql_database" "test" {
+  name                = "terraform-sqldatabase"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  server_name         = "${azurerm_sql_server.test.name}"
+
+  tags = {
+    environment = "production"
+  }
+}
