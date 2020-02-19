6556af23130bbd6f8e784d789f87f75adc32be9a
jfan <46353840+jeffan18@users.noreply.github.com>
2020-02-18 20:44:18 -0500

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -3,6 +3,28 @@ resource "azurerm_resource_group" "test" {
   location = "${var.location}"
 }
 
+resource "azurerm_sql_server" "test" {
+  name                         = "terraform-sqlserver"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  location                     = "${azurerm_resource_group.test.location}"
+  version                      = "12.0"
+  administrator_login          = "testtest"
+  administrator_login_password = "Henansheng!0"
+}
+
+resource "azurerm_sql_database" "test" {
+  name                = "terraform-sqldatabase"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  server_name         = "${azurerm_sql_server.test.name}"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+
+
 resource "azurerm_app_service_plan" "test" {
   name                = "example-appserviceplan"
   location            = "${azurerm_resource_group.test.location}"
@@ -36,22 +58,3 @@ resource "azurerm_app_service" "test" {
   }
 }
 
-resource "azurerm_sql_server" "test" {
-  name                         = "terraform-sqlserver"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  location                     = "${azurerm_resource_group.test.location}"
-  version                      = "12.0"
-  administrator_login          = "test"
-  administrator_login_password = "Henan$heng10"
-}
-
-resource "azurerm_sql_database" "test" {
-  name                = "terraform-sqldatabase"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  location            = "${azurerm_resource_group.test.location}"
-  server_name         = "${azurerm_sql_server.test.name}"
-
-  tags = {
-    environment = "production"
-  }
-}
